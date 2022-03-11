d8675e275ce451d8cfbdc6864478b548122f313d
TusharMittal585 <77103048+TusharMittal585@users.noreply.github.com>
2022-03-11 17:27:54 +0530

    Add files via upload

---
+++ 71/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyCWXfhpdM8U-Z8otUOdCOhC-1a1mZBPeC8",
+    authDomain: "e-ride-e4802.firebaseapp.com",
+    projectId: "e-ride-e4802",
+    storageBucket: "e-ride-e4802.appspot.com",
+    messagingSenderId: "529312548366",
+    appId: "1:529312548366:web:be2962b4503a736d409e5f"
+  };
+  
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
